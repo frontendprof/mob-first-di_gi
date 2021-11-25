aee38260bb0aba3e1262add8628e4604c518e7ba
frontendprof <https://github.com/frontendprof>
2021-11-25 14:25:13 +0500

    responsive banner

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ $(function () {
     dots: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><img src="./images/prev.svg" alt="prev_icon" /></button>',
+    nextArrow:
+      '<button class="slick-arrow slick-next"><img src="./images/next.svg" alt="next_icon" /></button>',
     responsive: [
       // {
       //   breakpoint: 1024,
